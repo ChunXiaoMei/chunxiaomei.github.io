9747e7b548aa1c531ffeed5da1711321dbc45285
zhangziliang <zhangziliang@jd.com>
2016-10-11 20:18:29 +0800

    .

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,34 @@
-console.log('This would be the main JS file.');
+$(function(){
+	var timer = null;
+	$('#openMenu').click(function(){
+		var iH = $(window).height();
+		
+		//alert(iH);
+		if($('#menu').css('display')=='none'){
+			var msk = $('<div id="msk"></div>');
+			
+			$('body').append(msk);			
+			$('#menu').show().animate({'right':0},500);
+		}else{
+			$('#menu').animate({'right':'-200px'},500);
+			
+			clearTimeout(timer);
+			timer = setTimeout(function(){
+				$('#msk').remove();
+				$('#menu').hide();
+			},500);
+		}
+	});
+	
+	$('#menu').swipeRight(function(){
+		$('#menu').animate({'right':'-200px'},500);
+			
+		clearTimeout(timer);
+		timer = setTimeout(function(){
+			$('#msk').remove();
+			$('#menu').hide();
+		},500);
+	});
+	
+});
+
